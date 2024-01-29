4d543b6d46c2ac05ea7fa506de61e38d1b0e2f1f
Carson Poole <carson.t.poole@gmail.com>
2024-01-28 22:58:50 -0500

    added license and copyright info

--- configs.cu
+++ configs.cu
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Forefront Industries, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #define TRANSFORM_N true
 #define TRANSFORM_T false
 #define LAYOUT_C true
@@ -1092,4 +1108,3 @@ constexpr KernelConfig mixtral_mlp_in_512 = {
     /* threadsPerBlock */ 256,
     /* pipelineStrat */ 2,
     /* paddingC */ 2};
-
